ce75325fb5b96cfbb40dfc91771a20584ef8e77c
lesqero <lesqero@users.noreply.github.com>
2023-10-25 10:41:51 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -8,7 +8,7 @@ class Header extends HTMLElement {
       
       <header class="container-fluid bg-earth_c">
         <div class="row">
-            <div class="col-auto">
+            <div class="col-auto d-none d-md-inline">
                 <a href="#" class="nav-link"><img src="img/header/angryhomebutton.png" style="max-height: 90px;"/></a>
             </div>
             <div class="col d-none d-md-inline">
@@ -23,6 +23,11 @@ class Header extends HTMLElement {
            <div class="col-auto py-3 px-1 d-none d-md-inline">
                 <a href="#" class="nav-link"><img src="img/header/contactButton.png" style="max-height: 60px;"/></a>
            </div>
+        </div>
+
+
+        <div class="col-auto d-md-none">
+          <a href="#" class="nav-link"><img src="img/header/angryhomebutton.png" class="mx-auto d-block" style="max-height: 90px;"/></a>
         </div>
 
         <div class="container">
