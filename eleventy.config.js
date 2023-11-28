99d2bc2787770aadcc7c1bb1cd0f33972fdf1d0c
lesqero <lm.walczuk@gmail.com>
2023-11-28 19:53:02 +0100

    working and fonts

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,7 +1,27 @@
 module.exports = function(eleventyConfig) {
+
+      // Copy `img/` to `_site/img`
+      eleventyConfig.addPassthroughCopy("components");
+
+      eleventyConfig.addPassthroughCopy("img");
+
+      eleventyConfig.addPassthroughCopy("css");
+
+      eleventyConfig.addPassthroughCopy("fonts");
+
+      eleventyConfig.addPassthroughCopy("about.html");
+
+      eleventyConfig.ignores.add("about.html");
+
+      eleventyConfig.addPassthroughCopy("contact.html");
+
+      eleventyConfig.ignores.add("contact.html");
+
     return {
       dir: {
         output: "docs"
       }
     }
+  
+
   };
